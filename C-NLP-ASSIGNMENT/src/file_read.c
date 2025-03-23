d9e2fa0e11ce36039a58c3f384ac8c33c44157ee
Vishal Narayan V <vishalnarayan2386@gmail.com>
2025-03-23 10:54:54 +0530

    Update file_read.c

--- C-NLP-ASSIGNMENT/src/file_read.c
+++ C-NLP-ASSIGNMENT/src/file_read.c
@@ -1,4 +1,4 @@
-#include "include/file_read.h"
+#include "/.../include/file_read.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,17 +9,37 @@ char** read(const char* filename,int* num_sentences){
     printf("ERROR OPENING FILE");
     exit(1);
   }
-
-  char** sentences=NULL;
-  char line[1000];
-  *num_sentences=0;
-
-  while(fgets(line,1000,file)){
-    sentences=realloc(sentences,(*num_sentences+1)*sizeof(char*));
-    line[strcspn(line,"\n")]='\0';
-    sentences[*num_sentences]=strdup(line);
-    (*num_sentences)++;
+  
+  char ch;
+  *(num_sentences)=0;
+  while((ch=fgetc(file))!=EOF){
+    if(ch=='\n'){
+      (*num_sentences)++;
+    }
   }
+  fseek(file, -1, SEEK_END);
+  if(fgetc(file)!='\n'){
+    (*num_sentences)++;
+    }
+  rewind(file);
+  
+  char** sentences=malloc((*num_sentences)*sizeof(char*));
+  if(!sentences){
+      printf("malloc failed\n");
+      exit(1);
+  }
+  size_t len=0;
+  ssize_t read;
+  char* line=NULL;
+  int index=0;
+  
+  while((read=getline(&line,&len,file))!=-1){
+      if(line[read-1]=='\n'){
+          line[read-1]='\0';
+      }
+      sentences[index++] = strdup(line);
+  }
+  free(line);
   fclose(file);
   return sentences;
 
